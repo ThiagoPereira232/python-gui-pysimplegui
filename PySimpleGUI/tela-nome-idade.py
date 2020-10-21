0547377a26a05f9a7e73063f0fffa3537897e81f
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-10-21 09:30:38 -0300

    Create tela-nome-idade.py

---
+++ PySimpleGUI/tela-nome-idade.py
@@ -0,0 +1,20 @@
+import PySimpleGUI as sg
+
+class TelaPython:
+    def __init__(self):
+        # layout
+        layout = [
+            [sg.Text('Nome'),sg.Input()],
+            [sg.Text('Idade'),sg.Input()],
+            [sg.Button('Enviar Dados')]
+        ]
+        # janela
+        janela = sg.Window("Dados do Usuário").layout(layout)
+        # extrair os dados da tela
+        self.button, self.values = janela.Read()
+
+    def Iniciar(self):
+        print(self.values)
+
+tela = TelaPython()
+tela.Iniciar()
