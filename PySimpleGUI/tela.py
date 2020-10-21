be2d91e1c995f9c0a7405474ae5ada69869010a1
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-10-21 10:18:17 -0300

    Update tela.py

--- PySimpleGUI/tela.py
+++ PySimpleGUI/tela.py
@@ -1,20 +1,47 @@
 import PySimpleGUI as sg
 
 class TelaPython:
+    sg.theme('Default')
+    # sg.change_look_and_feel('Default')
     def __init__(self):
         # layout
         layout = [
-            [sg.Text('Nome'),sg.Input()],
-            [sg.Text('Idade'),sg.Input()],
-            [sg.Button('Enviar Dados')]
+            [sg.Text('Nome',size=(5,0)),sg.Input(size=(15,0),key='nome')],
+            [sg.Text('Idade', size=(5,0)),sg.Input(size=(15,0),key='idade')],
+            [sg.Text('Quais provedores de e-mail são aceitos?')],
+            [sg.Checkbox('Gmail',key='gmail'),sg.Checkbox('Outlook',key='outlook'),sg.Checkbox('Yahoo',key='yahoo')],
+            [sg.Text('Aceita cartão')],
+            [sg.Radio('Sim','cartoes',key='aceitaCartao'),sg.Radio('Não','cartoes',key='naoAceitaCartao')],
+            [sg.Slider(range=(0,100),default_value=0,orientation='h',size=(15,20), key='sliderVelocidade')],
+            [sg.Button('Enviar Dados')],
+            [sg.Output(size=(30,20))]
         ]
         # janela
-        janela = sg.Window("Dados do Usuário").layout(layout)
-        # extrair os dados da tela
-        self.button, self.values = janela.Read()
+        self.janela = sg.Window("Dados do Usuário").layout(layout)
 
     def Iniciar(self):
-        print(self.values)
+        while True:
+            # extrair os dados da tela
+            self.button, self.values = self.janela.Read()
+
+            nome = self.values['nome']
+            idade = self.values['idade']
+            aceita_gmail = self.values['gmail']
+            aceita_outlook = self.values['outlook']
+            aceita_yahoo = self.values['yahoo']
+            aceita_cartao = self.values['aceitaCartao']
+            nao_aceita_cartao = self.values['naoAceitaCartao']
+            velocidade_script = self.values['sliderVelocidade']
+            print(f'Nome: {nome}')
+            print(f'Idade: {idade}')
+            print(f'Aceita gmail: {aceita_gmail}')
+            print(f'Aceita outlook: {aceita_outlook}')
+            print(f'Aceita yahoo: {aceita_yahoo}')
+            print(f'Aceita cartão: {aceita_cartao}')
+            print(f'não aceita cartão: {nao_aceita_cartao}')
+            print(f'Velocidade Scripts: {velocidade_script}')
+            print('----------------------------------------------------')
+
 
 tela = TelaPython()
 tela.Iniciar()
